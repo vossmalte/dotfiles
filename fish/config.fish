cfc1782b7c0b25e52903b6aab584e338c4fcf11b
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2024-01-23 23:52:30 +0100

    🔧 (chore): update PATH

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path /opt/homebrew/bin
     fish_add_path ~/.n/bin
+    fish_add_path ~/.ghcup/bin
+    fish_add_path ~/.cabal/bin
+    fish_add_path ~/.local/bin
     source (/opt/homebrew/bin/starship init fish --print-full-init | psub)
 end
